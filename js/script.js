6e022c1a2e128bd70dcf33aeddc7aabb1db1a053
Tyler Akin <tyler@appriss.com>
2021-11-22 19:50:06 -0500

    Adds entire calculator js

--- js/script.js
+++ js/script.js
@@ -3,26 +3,44 @@ var number1 = document.getElementById('number-1');
 var number2 = document.getElementById('number-2');
 var the_result = document.getElementById('the_result');
 var calculate_button = document.getElementById('calculate');
+var operator_options = document.getElementById('operator_options');
+var the_current_operator_type = 'add';
+var number1Value = 0;
+var number2Value = 0;
 
-console.log(number1, number2, the_result, calculate_button)
-
-function outPutTheCurrentValue(theInputNumber, thePassedInValue) {
-    console.log('number ' + theInputNumber + ' input value is changing.', thePassedInValue)
+function calculateTheValue(first_field_to_calculate, second_field_to_calculate, operator_type) {
+    if (operator_type === 'add') {
+        return first_field_to_calculate + second_field_to_calculate;
+    } else if (operator_type === 'subtract') {
+        return first_field_to_calculate - second_field_to_calculate;
+    } else if (operator_type === 'divide') {
+        return first_field_to_calculate / second_field_to_calculate;
+    } else if (operator_type === 'multiply') {
+        return first_field_to_calculate * second_field_to_calculate;
+    } else {
+        return;
+    }
 }
 
-number1.addEventListener('change', function (e) {
-    outPutTheCurrentValue(1, e.target.value)
+function outputTheCorrectValue(first_field_value, second_field_value, operator) {
+    var theOperationValue = calculateTheValue(first_field_value, second_field_value, operator);
+
+    the_result.innerHTML = theOperationValue;
+}
+
+operator_options.addEventListener('change', function (e) {
+    the_current_operator_type = e.target.value;
 });
-
-
-number2.addEventListener('change', function (e) {
-    outPutTheCurrentValue(2, e.target.value)
-});
-
 
 calculate_button.addEventListener('click', function () {
-    var number1Value = number1.value;
-    var number2Value = number2.value;
 
-    the_result.innerHTML = number1Value * number2Value;
+    if (number1.value) {
+        number1Value = parseInt(number1.value);
+    }
+
+    if (number2.value) {
+        number2Value = parseInt(number2.value);
+    }
+
+    outputTheCorrectValue(number1Value, number2Value, the_current_operator_type);
 });
